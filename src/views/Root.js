522be71079aa7535549196c6e69b05467a1b6d79
mateuszral <mateuszral7@gmail.com>
2021-12-07 18:28:22 +0100

    gh pages update

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-import { routes } from 'routes';
 
 import MainTemplate from 'templates/MainTemplate';
 
 import Home from 'views/Home';
 
 const Root = () => (
-  <BrowserRouter>
-    <MainTemplate>
-      <Switch>
-        <Route exact path={routes.home} component={Home} />
-      </Switch>
-    </MainTemplate>
-  </BrowserRouter>
+  <MainTemplate>
+    <Home />
+  </MainTemplate>
 );
 
 export default Root;
